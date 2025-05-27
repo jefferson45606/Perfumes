deca2869a444b4b57b9f1d4309305e46917e0f14
jefferson45606 <jefferson240111@gmail.com>
2025-05-26 21:21:46 -0500

    3.1
    cambio al backend

--- perfumeria/index.php
+++ perfumeria/index.php
@@ -1,5 +1,24 @@
 <?php
-session_start();
+include("includes/conexion.php");
+
+$msg= '';
+if(isset($_POST['registrar'])){
+    $usuario=$_POST['usuario'];
+    $contraseña=$_POST['password'];
+
+    $select1="SELECT * FROM `administrador` WHERE usuario='$usuario' AND contraseña='$password'";
+    $select_user=mysqli_query($conn, $select1);
+
+    if(mysqli_num_rows($select_user) > 0){
+        $row1=mysqli_fetch_assoc($select_user);
+        if($row1['usuario']=='administrador'){
+            $_SESSION['administrador']=$row1['usuario'];
+            $_SESSION['contraseña']=$row1['contraseña'];
+            header("location:vender.html");
+        }
+        
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,24 +37,23 @@ session_start();
             <img src="IMG/Su_Aroma.png" alt="Logo Su Aroma" class="logo">
         </div>
 
-
         <div class="login-section">
-            <h2 class="title">BIENVENIDA</h2>
-            <p class="subtitle">Inicia sesión para continuar.</p>
-            <form action="login.php" method="post"> 
+            <form action="" method="post"></form> <!-- metodo post -->
+                <h2 class="title">BIENVENIDA</h2>
+                <p class="subtitle">Inicia sesión para continuar.</p>
                 
                 <label for="usuario">USUARIO</label>
                 <input type="text" id="usuario" name="usuario" placeholder="yamile bruno" required>
                 
                 <label for="password">CONTRASEÑA</label>
-                <input type="password" id="password" name="contraseña" placeholder="12345" required>
+                <input type="password" id="password" name="password" placeholder="12345" required>
 
                 <div class="show-password">
                     <input type="checkbox" id="togglePassword">
                     <label for="togglePassword">Mostrar contraseña</label>
                 </div>
                 
-                <button type="submit">INGRESAR</button>
+                <button class="btn font-weight-bold" name="registrar">INGRESAR</button>
                 
                 <a href="recuperar.html">¿Has olvidado tu contraseña?</a>
             </form>
@@ -50,17 +68,5 @@ session_start();
             passwordInput.type = this.checked ? 'text' : 'password';
         });
     </script>
-
-    <?php
-    if (isset($_GET['error'])){
-        
-        if ($_GET['error'] == 'campos') {
-            echo "<p style='color:red;'>Por favor complete todos los campos.</p>";
-        } elseif ($_GET['error'] == 'credenciales') {
-            echo "<p style='color:red;'>Usuario o contraseña incorrectos.</p>";
-        }
-    }
-    ?>
-
 </body>
 </html>
